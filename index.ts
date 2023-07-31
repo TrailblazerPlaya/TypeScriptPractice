d4619036246ac2aaa311322a1590220ddbbf5b20
fornara <fornarapro@gmail.com>
2023-07-31 14:14:56 +0300

    tsBaseTypes
    базовые типы в TypeScript

---
+++ index.ts
@@ -0,0 +1,9 @@
+// let username: string = "John";
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = "John";
+age = 22;
+if (isBirthday) {
+    console.log(`Happy Birthday ${userName.toUpperCase()}, age: ${age + 1}`);
+}
