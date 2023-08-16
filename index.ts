1003ea1ce4f13cebc5eadb4ddf829e3edfc337b1
fornara <fornarapro@gmail.com>
2023-08-16 13:36:42 +0300

    AbstractVehicleAndStack
    practice

--- index.ts
+++ index.ts
@@ -1012,25 +1012,166 @@
 
 
 ////lesson 75
-interface IEngine {
-	model: string;
-	capacity: number;
-	startEngine: (time: Date) => string;
+// interface IEngine {
+// 	model: string;
+// 	capacity: number;
+// 	startEngine: (time: Date) => string;
+// }
+
+// abstract class AbstractVehicle {
+// 	model: string;
+// 	capacity: number;
+// 	abstract startEngine: (time: Date) => string;
+// 	stopEngine(time: Date): string	{
+// 		return "Stop";
+// 	}
+// }
+
+// class Vehicle extends AbstractVehicle {
+// 	startEngine = (time: Date) => {
+// 		return "Start";
+// 	};
+// }
+
+// new Vehicle();
+
+//////////////lesson 76
+interface Queue<T> {
+	enqueue(item: T): void; // поставить в очередь
+	dequeue(): T | undefined; // исключить из очереди
+	peek(): T | undefined | null; // посмотреть первый элемент
+	isEmpty(): boolean; // проверка на "пустоту" сущности
+	length(): number; // проверка на длину
 }
 
-abstract class AbstractVehicle {
-	model: string;
-	capacity: number;
-	abstract startEngine: (time: Date) => string;
-	stopEngine(time: Date): string	{
-		return "Stop";
+// Реализация очереди через массив
+// Класс ArrayQueue должен имплементировать интерфейс Queue
+// Класс может работать с любым типом данных, то есть помещать любые данные в массив  <-- Важно
+
+// Очередь - это структура данных, которая выглядит как реальная очередь в магазине
+// Первый, кто подошел к прилавку, первым и уйдет. Так же и в коде при выполнении задач
+// Чуть подробнее можно найти в википедии или на других сайтах по поиску "Очередь структура данных"
+
+class ArrayQueue<T> implements Queue<T> {
+	// Создать приватное свойство queue, которое по умолчанию массив и содержит массив любого типа
+	private queue: T[] = [];
+	// Подсказка по методам:
+	// при добавлении в очередь можно выполнить метод push
+	enqueue(this: ArrayQueue<T>, item: T): void {
+		this.queue.push(item);
+	}
+	dequeue(this: ArrayQueue<T>) : T {
+		if (this.isEmpty()) {
+			throw new Error("Queue Underflow");
+		}
+
+		return this.queue.shift() as T;
+	}
+
+	peek(this: ArrayQueue<T>): T | null {
+		if (this.isEmpty()) {
+			return null;
+		}
+
+		return this.queue[0];
+	}
+	// при удалении - shift, так как нужно удалить первый элемент.
+	isEmpty(this: ArrayQueue<T>): boolean {
+		return this.queue.length === 0;
+	}
+	// Обратите внимание на возвращаемое значение
+	// isEmpty может использоваться в других методах
+	length(this: ArrayQueue<T>): number {
+		return this.queue.length;
 	}
 }
 
-class Vehicle extends AbstractVehicle {
-	startEngine = (time: Date) => {
-		return "Start";
-	};
+// Стэк - это еще одна структура данных. Проще всего её представить как стопку листов на столе
+// Последний, который вы положите сверху, вы и первым потом возьмете.
+// Чуть подробнее можно найти в википедии или на других сайтах по поиску "Стэк структура данных"
+// Класс Stack содержит другие методы, так что ничего имплементировать не нужно
+// Класс может работать с любым типом данных, то есть помещать любые данные в массив и содержит массив любого типа  <-- Важно
+
+class Stack <T> {
+	private stack : T[] = [];
+	// Создать приватное свойство stack, которое по умолчанию массив и содержит массив любого типа
+	// Создать приватное свойство limit, которое будет типом number
+	private limit: number;
+	// Здесь мы установим лимит на стопку листов.
+	// При переполнении стэка программа зависает, а очень высокая стопка листов падает
+	// Так что лимит всегда должен быть
+	constructor(limit: number = Number.MAX_VALUE) {
+		this.limit = limit;
+	}
+
+	push(this: Stack<T>, value: T) {
+		// Добавляет элемент в стэк
+		if (this.length() + 1 > this.limit) {
+			throw new Error("Stack Overflow");
+		}
+		this.stack.push(value);
+		// Если стэк переполнен - выбрасывает ошибку (throw new Error)
+	}
+
+	pop(this: Stack<T>) : T {
+		// Удаляет последний элемент массива
+		if (this.length() !== 0) {
+			return this.stack.pop() as T;
+		}
+		throw new Error("Stack underflow");
+		// Если в стеке пусто - выбрасывает ошибку (throw new Error)
+		// При удалении элемента возвращает его
+		// Простыми словами: вы берете верхний лист в стопке и используете его
+		// Если на столе нет листов - получается ошибка, брать нечего
+	}
+
+	length(this: Stack<T>): number {
+		// Возвращает кол-во элементов в стэке
+		return this.stack.length;
+	}
+
+	isEmpty(this: Stack<T>): boolean {
+		// Проверяет стэк на "пустоту"
+		return this.stack.length === 0;
+	}
+
+	top(this: Stack<T>) : T | null {
+		// Возвращает последний (верхний) элемент стэка, но не удаляет его
+		if (this.length() !== 0) {
+			return this.stack[this.stack.length - 1];
+		}
+		// Вы просто читаете, что написано на верхнем листе
+		// Если стэк пуст - вернется null
+		return null;
+	}
 }
 
-new Vehicle();
+// Для тестов
+
+const arrTest1 = new ArrayQueue<number>();
+arrTest1.enqueue(5);
+arrTest1.enqueue(10);
+console.log(arrTest1.peek());
+console.log(arrTest1.dequeue());
+console.log(arrTest1.length());
+
+const arrTest2 = new ArrayQueue<string>();
+arrTest2.enqueue("5");
+arrTest2.enqueue("10");
+console.log(arrTest2.peek());
+console.log(arrTest2.dequeue());
+console.log(arrTest2.length());
+
+const stackTest1 = new Stack<number>(10);
+stackTest1.push(20);
+stackTest1.push(50);
+console.log(stackTest1.top());
+console.log(stackTest1.pop());
+console.log(stackTest1.length());
+
+const stackTest2 = new Stack<string>(10);
+stackTest2.push("20");
+stackTest2.push("50");
+console.log(stackTest2.top());
+console.log(stackTest2.pop());
+console.log(stackTest2.length());
