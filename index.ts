1fbf596ee606cf2126d7580b01fc1d97410e5cf0
fornara <fornarapro@gmail.com>
2023-08-19 15:43:34 +0300

    typeGuardAgain
    начал еще один курс по TS для закрепления материала

--- index.ts
+++ index.ts
@@ -1,10 +1,64 @@
-/* 
-Осталось пройти функции декораторы
-Я пытался решить практику не зная этой темы, потом что уроки были перепутаны и я не понял это сразу
-Оцениваю данный курс на 5/10
-Слишком все запутанно и практика максимально нудная
-У курса есть хорошие конспекты, которые я буду использовать в дальнейшем 
-Пройду еще один курс по TS в ускоренном темпе чтобы закрепить в голове темы
+interface IPayment {
+	sum: number;
+	from: number;
+	to: number;
+}
 
+enum PaymentStatus {
+	Success = 'success',
+	Failed = 'failed',
+}
 
-*/ 
+interface IPaymentRequest extends IPayment { }
+
+interface IDataSuccess extends IPayment {
+	databaseId: number;
+}
+
+interface IDataFailed {
+	errorMessage: string;
+	errorCode: number;
+}
+
+interface IResponseSuccess {
+	status: PaymentStatus.Success;
+	data: IDataSuccess;
+}
+
+interface IResponseFailed {
+	status: PaymentStatus.Failed;
+	data: IDataFailed;
+}
+
+function isResponseSuccess(response: IResponseSuccess | IResponseFailed): response is IResponseSuccess {
+    return response.status === PaymentStatus.Success;
+}
+
+function processResponse(response: IResponseSuccess | IResponseFailed) {
+    if (isResponseSuccess(response)) {
+        console.log('Успешный платеж', PaymentStatus.Success);
+    } else {
+        console.log(PaymentStatus.Failed);
+    }
+}
+
+const successResponse: IResponseSuccess = {
+    status: PaymentStatus.Success,
+    data: {
+        sum: 100,
+        from: 1,
+        to: 2,
+        databaseId: 1,
+    },
+}
+
+const failedResponse: IResponseFailed = {
+    status: PaymentStatus.Failed,
+    data: {
+        errorMessage: 'Error',
+        errorCode: 1,
+    },
+}
+
+processResponse(successResponse);
+processResponse(failedResponse);
