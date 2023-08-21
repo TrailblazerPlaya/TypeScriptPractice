ca14c2d889f2ac3c1af9c95fab06c278c1716b68
fornara <fornarapro@gmail.com>
2023-08-21 16:13:47 +0300

    cartPractice
    создание корзины товаров

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-"use strict";
-class User {
-    // addSkill(skill: string): void {}; я добавил в конец скобки и у меня была ошибка
-    // addSkill(skills: string[]): void {};
-    addSkill(skillOrSkills) {
-        if (typeof skillOrSkills == 'string') {
-            this.skills.push(skillOrSkills);
-        }
-        else {
-            this.skills.concat(skillOrSkills);
-        }
+/*Необходимо сделать корзину (Cart) на сайте,
+которая имееет список продуктов (Product), добавленных в корзину
+и переметры доставки (Delivery). Для Cart реализовать методы:
+- Добавить продукт в корзину
+- Удалить продукт из корзины по ID
+- Посчитать стоимость товаров в корзине
+- Задать доставку
+- Checkout - вернуть что всё ок, если есть продукты и параметры доставки
+Product: id, название и цена
+Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)*/
+var Product = /** @class */ (function () {
+    function Product(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
-}
-new User().addSkill(['js', 'ts']);
-class Calculator {
-    add(a, b) {
-        if (typeof a === 'number' && typeof b === 'number') {
-            return a + b;
-        }
-        else if (typeof a === 'string' && typeof b === 'string') {
-            return a.concat(b);
-        }
-        else {
-            throw new Error('Invalid arguments');
-        }
+    return Product;
+}());
+var Delivery = /** @class */ (function () {
+    function Delivery(date, address, storeId) {
+        this.date = date;
+        this.address = address;
+        this.storeId = storeId;
     }
-}
+    return Delivery;
+}());
+var Card = /** @class */ (function () {
+    function Card() {
+        this.product = [];
+    }
+    Card.prototype.addProduct = function (product) {
+        this.product.push(product);
+        console.log('Product added');
+    };
+    Card.prototype.removeProductById = function (id) {
+        this.product = this.product.filter(function (product) { return product.id !== id; });
+    };
+    Card.prototype.calculateTotalPrice = function () {
+        return this.product.reduce(function (total, product) { return total + product.price; }, 0);
+    };
+    Card.prototype.setDelivery = function (delivery) {
+        this.delivery = delivery;
+        console.log('Delivery set');
+    };
+    Card.prototype.checkout = function () {
+        return this.product.length > 0 && this.delivery !== undefined;
+    };
+    return Card;
+}());
+var product1 = new Product(1, "Product 1", 100);
+var product2 = new Product(2, "Product 2", 200);
+var delivery1 = new Delivery(new Date('2023-08-21'), 'Address 1');
+var delivery2 = new Delivery(new Date(), undefined, 1);
+var cart = new Card();
+cart.addProduct(product1);
+cart.addProduct(product2);
+cart.setDelivery(delivery1);
--- index.ts
+++ index.ts
@@ -1,35 +1,71 @@
-class User {
-    skills: string[];
-    addSkill(skill: string): void;
-    addSkill(skills: string[]): void;
-    // addSkill(skill: string): void {}; я добавил в конец скобки и у меня была ошибка
-    // addSkill(skills: string[]): void {};
-    addSkill(skillOrSkills: string | string[]): void {
-        if (typeof skillOrSkills == 'string') {
-            this.skills.push(skillOrSkills);
-        } else {
-            this.skills.concat(skillOrSkills);
-        }
+/*Необходимо сделать корзину (Cart) на сайте,
+которая имееет список продуктов (Product), добавленных в корзину
+и переметры доставки (Delivery). Для Cart реализовать методы:
+- Добавить продукт в корзину
+- Удалить продукт из корзины по ID
+- Посчитать стоимость товаров в корзине
+- Задать доставку
+- Checkout - вернуть что всё ок, если есть продукты и параметры доставки
+Product: id, название и цена
+Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)*/
 
+class Product {
+    id: number;
+    name: string;
+    price: number;
+    constructor(id: number, name: string, price: number) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
-        
-    
+}       
+
+class Delivery {
+    date: Date;
+    address?: string;
+    storeId?: number;
+    constructor(date: Date, address?: string, storeId?: number) {
+        this.date = date;
+        this.address = address;
+        this.storeId = storeId;
+      }
 }
 
-new User().addSkill(['js', 'ts']);
-
-
-
-class Calculator {
-    add(a: number, b: number): number;
-    add(a: string, b: string): string;
-    add(a: any, b: any): any {
-      if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-      } else if (typeof a === 'string' && typeof b === 'string') {
-        return a.concat(b);
-      } else {
-        throw new Error('Invalid arguments');
-      }
+class Card {
+    product: Product[] = [];
+    delivery?: Delivery;
+    
+    addProduct(product: Product) : void {
+        this.product.push(product);
+        console.log('Product added');
     }
-  }
+
+    removeProductById(id: number) : void {
+        this.product = this.product.filter(product => product.id !== id);
+    }
+
+    calculateTotalPrice() : number {
+        return this.product.reduce((total, product) => total + product.price, 0);
+    }
+
+    setDelivery(delivery: Delivery) : void {
+        this.delivery = delivery;
+        console.log('Delivery set');
+    }
+
+    checkout() : boolean {
+        return this.product.length > 0 && this.delivery !== undefined;
+    }
+}
+
+const product1 = new Product(1, "Product 1", 100);
+const product2 = new Product(2, "Product 2", 200);
+
+const delivery1 = new Delivery(new Date('2023-08-21'), 'Address 1');
+const delivery2 = new Delivery(new Date(), undefined, 1);
+
+const cart = new Card();
+
+cart.addProduct(product1);
+cart.addProduct(product2);
+cart.setDelivery(delivery1);
