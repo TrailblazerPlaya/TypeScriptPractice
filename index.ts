51be6e9bf45106db85985f3d30a7ee01a9b3644f
fornara <fornarapro@gmail.com>
2023-08-29 15:40:51 +0300

    decoratorCreateAt

--- index.ts
+++ index.ts
@@ -1,31 +1,28 @@
-/** 
- * Необходимо сделать тип для результатов валидации формы, основываясь на типе формы 
+/**
+ * Декоратор, который добавляет свойство createdAt в класс, фиксируя дату создания
+ * 
  */
 
-interface IForm {
-    name: string;
-    password: string;
+interface IUserService  {
+    users: number;
+    getUserInDatabase(): number;
 }
+@CreatedAt //experimentalDecorators: true иначе будет ошибка
+class UserService implements IUserService  {
+    users: number = 1000;
+    getUserInDatabase(): number {
+        return this.users;
+    }
+}    
 
-type FormValidationResult<T extends Record<string, any>> = {
-    // [K in keyof T]: {
-    //     isValue: boolean;
-    //     errorMassage?: string;
-    // };
-    [K in keyof T] : {
-        isValue: true;
-    } | {
-        isValue: false;
-        errorMassage: string;
+function CreatedAt<T extends { new (...args: any[]): {}}>(constructor: T) {
+    return class extends constructor {
+        createdAt: Date = new Date();
     }
 }
-const form: IForm = {
-    name: 'Kolya',
-    password: 'asdasd'
+
+type CreatedAt = {
+    createdAt: Date
 }
 
-const formValidation: FormValidationResult<IForm> = {
-    name: { isValue: true },
-    password : { isValue: false, errorMassage: 'должен быть больше 5 символов' }
-}
-/**Здесь мы определяем тип  FormValidationResult<T> , который использует Mapped Types для создания объекта с ключами из типа  T  (в данном случае  IForm ). Каждому ключу соответствует объект с двумя свойствами:  isValue  типа  boolean  и, при необходимости,  errorMessage  типа  string . Таким образом, вы можете использовать тип  FormValidationResult<IForm>  для представления результатов валидации формы с указанием, заполнено ли поле ( isValue ) и, при необходимости, сообщения об ошибке ( errorMessage ). */
+console.log((new UserService() as IUserService & CreatedAt).getUserInDatabase());
